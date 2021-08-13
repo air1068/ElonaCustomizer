cf9d0f352281aa282cdca1c1bfbf6de08dcdc232
air1068 <slashangemon@gmail.com>
2021-08-12 19:54:07 -0500

    Update to v0.8a
    Adds support for "relative labels" in ECL files, auto-removes some debug labels, and adds ECL file verification

--- MainWindow.java
+++ MainWindow.java
@@ -11,6 +11,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
@@ -83,8 +85,8 @@ public class MainWindow {
 	 */
 	protected void createContents() {
 		shlElonaCustomizer = new Shell();
-		shlElonaCustomizer.setSize(450, 160);
-		shlElonaCustomizer.setText("Elona Customizer v0.7b");
+		shlElonaCustomizer.setSize(450, 175);
+		shlElonaCustomizer.setText("Elona Customizer v0.8a");
 		
 		Label lblNewLabel = new Label(shlElonaCustomizer, SWT.NONE);
 		lblNewLabel.setBounds(189, 37, 235, 15);
@@ -94,8 +96,52 @@ public class MainWindow {
 		lblCustomLabelFile.setBounds(91, 37, 92, 15);
 		
 		ProgressBar progressBar = new ProgressBar(shlElonaCustomizer, SWT.NONE);
-		progressBar.setBounds(0, 103, 434, 17);
+		progressBar.setBounds(0, 119, 434, 17);
 		
+		Button btnVerifyEclFile = new Button(shlElonaCustomizer, SWT.NONE);
+		btnVerifyEclFile.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				progressBar.setSelection(0);
+				File hsploc = new File(text.getText());
+				File ecl = new File(getFolder(hsploc), hsploc.getName().toLowerCase().replaceFirst("hsp$", "ecl"));
+				File result = new File(getFolder(hsploc), hsploc.getName().toLowerCase().replaceFirst(".hsp$", " verification log.txt"));
+				try {
+					result.createNewFile();
+					BufferedReader hspr = new BufferedReader(new InputStreamReader(new FileInputStream(hsploc), "Shift_JIS"));
+					String mainhsp = hspr.lines().collect(Collectors.joining("\n"));
+					BufferedReader eclr = new BufferedReader(new InputStreamReader(new FileInputStream(ecl), "Shift_JIS"));
+					BufferedWriter rw = new BufferedWriter(new FileWriter(result));
+					rw.write("Checking standard label entries in "+ecl.getName()+"...\n");
+					progressBar.setSelection(50);
+					String line = eclr.readLine();
+					while (line != null) {
+						if (line.startsWith("*")) {
+							//skip relative labels
+							line = eclr.readLine();
+						} else {							
+							line = eclr.readLine();
+							int count = StringUtils.countMatches(mainhsp, line);
+							if (count != 1) {
+								rw.write(count + ": " + line + "\n");
+							}
+						}
+						line = eclr.readLine();
+					}
+					rw.write("Complete! Any search strings that weren't found to have exactly one match are listed above.");
+					hspr.close();
+					eclr.close();
+					rw.close();
+				} catch (IOException e1) {
+					errorMessage(e1.getClass().toString(), e1.getMessage());
+				}
+				progressBar.setSelection(100);
+			}
+		});
+		btnVerifyEclFile.setEnabled(false);
+		btnVerifyEclFile.setBounds(341, 88, 83, 25);
+		btnVerifyEclFile.setText("Verify ECL File");
+
 		Button btnOpen = new Button(shlElonaCustomizer, SWT.NONE);
 		btnOpen.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -112,8 +158,10 @@ public class MainWindow {
 					lblCustomLabelFile.setText("Custom label file: ");
 					if (ecl.exists()) {
 						lblNewLabel.setText(ecl.getName());
+						btnVerifyEclFile.setEnabled(true);
 					} else {
 						lblNewLabel.setText("None");
+						btnVerifyEclFile.setEnabled(false);
 					}
 				}
 			}
@@ -178,19 +226,40 @@ public class MainWindow {
 				catch (IOException e1) {
 					errorMessage(e1.getClass().toString(), e1.getMessage());
 				}
-				progressBar.setSelection(25);
+				progressBar.setSelection(20);
+				//Auto-remove debug goto labels pointing to function declarations.
+				mainhsp = mainhsp.replaceAll("\n\\*label_[0-9][0-9][0-9][0-9]\n#def", "\n#def");
+				//"Merge" successive labels, since it's random whether the top one or bottom one is the debug label.
+				String[] searchList = {};
+				String[] replacementList = {};	
+				Pattern debuglabel = Pattern.compile("\\n(\\*label_[0-9][0-9][0-9][0-9])\\n(\\*label_[0-9][0-9][0-9][0-9])");
+				Matcher m = debuglabel.matcher(mainhsp);
+				while (m.find()) {
+					searchList = ArrayUtils.add(searchList, m.group(1));
+					replacementList = ArrayUtils.add(replacementList, m.group(2));
+					mainhsp = mainhsp.substring(0, m.start()) + mainhsp.substring(mainhsp.indexOf("\n", m.start()+1));
+				}
+				mainhsp = StringUtils.replaceEach(mainhsp, searchList, replacementList);
+				progressBar.setSelection(40);
 				if (lblNewLabel.getText() != "None") {
-					String[] searchList = {};
-					String[] replacementList = {};
+					searchList = new String[] {};
+					replacementList = new String[] {};
+					String[] relativeLabels = {};
 					File eclloc = new File(getFolder(hsploc), lblNewLabel.getText());
 					try {
 						BufferedReader br = Files.newBufferedReader(eclloc.toPath(), Charset.forName("Shift_JIS"));
 						String line = br.readLine();
 						while (line != null) {
-							replacementList = ArrayUtils.add(replacementList, line);
-							int indexa = mainhsp.lastIndexOf("\n*", mainhsp.indexOf(br.readLine()));
-							int indexb = mainhsp.indexOf("\n", indexa+1);
-							searchList = ArrayUtils.add(searchList, mainhsp.substring(indexa+2, indexb-1));
+							if (line.startsWith("*")) {
+								//Relative labels can't be handled until after normal labels are done.
+								relativeLabels = ArrayUtils.add(relativeLabels, line.substring(1));
+								relativeLabels = ArrayUtils.add(relativeLabels, br.readLine());
+							} else {
+								replacementList = ArrayUtils.add(replacementList, line);
+								int indexa = mainhsp.lastIndexOf("\n*", mainhsp.indexOf(br.readLine()));
+								int indexb = mainhsp.indexOf("\n", indexa+1);
+								searchList = ArrayUtils.add(searchList, mainhsp.substring(indexa+2, indexb));
+							}
 							line = br.readLine();
 						}
 						br.close();
@@ -199,10 +268,15 @@ public class MainWindow {
 					} catch (IOException e1) {
 						errorMessage(e1.getClass().toString(), e1.getMessage());
 					}
-					progressBar.setSelection(50);
 					mainhsp = StringUtils.replaceEach(mainhsp, searchList, replacementList);
+					for (int i = 0; i < relativeLabels.length; i = i + 2) {
+						int indexa = mainhsp.indexOf("\n*", mainhsp.indexOf(relativeLabels[i+1]));
+						int indexb = mainhsp.indexOf("\n", indexa+1);
+						mainhsp = mainhsp.replace(mainhsp.substring(indexa+2, indexb), relativeLabels[i]);
+					}
+					progressBar.setSelection(60);
 				}
-				progressBar.setSelection(75);
+				progressBar.setSelection(80);
 				File splitfile = new File(getFolder(hsploc), hsploc.getName().toLowerCase().replaceFirst("hsp$", "")+"split.hsp");
 				try {
 					splitfile.createNewFile();
